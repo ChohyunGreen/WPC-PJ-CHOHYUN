adf5db8ebb29d3eb0b1f4416db857161edfe8a1f
ChohyunGreen <texascitycho@gmail.com>
2025-09-29 17:52:57 +0900

    zz

--- 1차_웹프로젝트/project/js/main_dbh.js
+++ 1차_웹프로젝트/project/js/main_dbh.js
@@ -1,3 +1,71 @@
+        const dropdown = document.querySelector('.dropdown');
+        const dropdownButton = document.querySelector('.dropdown-button');
+        const closeButton = document.querySelector('.close-button');
+
+        dropdownButton.addEventListener('click', function() {
+            dropdown.classList.toggle('active');
+        });
+
+        closeButton.addEventListener('click', function() {
+            dropdown.classList.remove('active');
+        });
+
+        // 메뉴 외부 클릭 시 닫기
+        document.addEventListener('click', function(event) {
+            const dropdownMenu = document.querySelector('.dropdown-menu');
+            if (!dropdown.contains(event.target) && !dropdownMenu.contains(event.target)) {
+                dropdown.classList.remove('active');
+            }
+        });
+  
+
+        var gk_isXlsx = false;
+        var gk_xlsxFileLookup = {};
+        var gk_fileData = {};
+        function filledCell(cell) {
+          return cell !== '' && cell != null;
+        }
+        function loadFileData(filename) {
+        if (gk_isXlsx && gk_xlsxFileLookup[filename]) {
+            try {
+                var workbook = XLSX.read(gk_fileData[filename], { type: 'base64' });
+                var firstSheetName = workbook.SheetNames[0];
+                var worksheet = workbook.Sheets[firstSheetName];
+
+                // Convert sheet to JSON to filter blank rows
+                var jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1, blankrows: false, defval: '' });
+                // Filter out blank rows (rows where all cells are empty, null, or undefined)
+                var filteredData = jsonData.filter(row => row.some(filledCell));
+
+                // Heuristic to find the header row by ignoring rows with fewer filled cells than the next row
+                var headerRowIndex = filteredData.findIndex((row, index) =>
+                  row.filter(filledCell).length >= filteredData[index + 1]?.filter(filledCell).length
+                );
+                // Fallback
+                if (headerRowIndex === -1 || headerRowIndex > 25) {
+                  headerRowIndex = 0;
+                }
+
+                // Convert filtered JSON back to CSV
+                var csv = XLSX.utils.aoa_to_sheet(filteredData.slice(headerRowIndex)); // Create a new sheet from filtered array of arrays
+                csv = XLSX.utils.sheet_to_csv(csv, { header: 1 });
+                return csv;
+            } catch (e) {
+                console.error(e);
+                return "";
+            }
+        }
+        return gk_fileData[filename] || "";
+        }
+
+
+
+
+
+
+
+
+
 // ● 상단카테고리 (마우스오버시 하위메뉴 노출)
 $(document).ready(function() {
     $('.cate > li').hover(function() {
@@ -12,6 +80,7 @@ $(document).ready(function() {
         $(this).find('ul.sub_dep_2').stop(true, true).slideUp(200);
     });
 });
+
 
 
 
